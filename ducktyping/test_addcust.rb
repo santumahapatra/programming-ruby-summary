074882f1dd90675d36c4ca3414116203ffc31f93
Santu Mahapatra <santumahapatra@gmail.com>
2014-09-25 17:30:06 +0530

    Using ducktyping. Passing a string instead of a file.

--- ducktyping/test_addcust.rb
+++ ducktyping/test_addcust.rb
@@ -4,13 +4,8 @@ require_relative 'addcust'
 class TestAddCustomer < Test::Unit::TestCase
   def test_add
     c = Customer.new("Ima", "Customer")
-    f = File.open("tmpfile", "w") do |f|
-      c.append_name_to_file(f)
-    end
-    f = File.open("tmpfile") do |f|
-      assert_equal("Ima Customer", f.gets)
-    end
-  ensure
-    File.delete("tmpfile") if File.exist?("tmpfile")
+    f = ""
+    c.append_name_to_file(f)
+    assert_equal("Ima Customer", f)
   end
 end
