0bab385395449b23f0fdc898959f02a7c50c4a56
Santu Mahapatra <santumahapatra@gmail.com>
2014-09-25 17:26:43 +0530

    Example of a test writing into a file and checking the output.

---
+++ ducktyping/test_addcust.rb
@@ -0,0 +1,16 @@
+require 'test/unit'
+require_relative 'addcust'
+
+class TestAddCustomer < Test::Unit::TestCase
+  def test_add
+    c = Customer.new("Ima", "Customer")
+    f = File.open("tmpfile", "w") do |f|
+      c.append_name_to_file(f)
+    end
+    f = File.open("tmpfile") do |f|
+      assert_equal("Ima Customer", f.gets)
+    end
+  ensure
+    File.delete("tmpfile") if File.exist?("tmpfile")
+  end
+end
