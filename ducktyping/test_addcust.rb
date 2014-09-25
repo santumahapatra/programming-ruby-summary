233c78baf062d323aa4c6dcf894046b0b9b55127
Santu Mahapatra <santumahapatra@gmail.com>
2014-09-25 17:31:33 +0530

    Using an array instead of a file.

--- ducktyping/test_addcust.rb
+++ ducktyping/test_addcust.rb
@@ -4,8 +4,8 @@ require_relative 'addcust'
 class TestAddCustomer < Test::Unit::TestCase
   def test_add
     c = Customer.new("Ima", "Customer")
-    f = ""
+    f = []
     c.append_name_to_file(f)
-    assert_equal("Ima Customer", f)
+    assert_equal(["Ima", " ", "Customer"], f)
   end
 end
